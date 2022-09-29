c0dd2782778c59e753b23bf67a7e9e373aed9f8b
ndimitrova <nninata@abv.bg>
2022-09-29 14:23:02 +0300

    First Step in Coding - More Exercise/CelsiusТoFahrenheit.js

---
+++ First Ster in Coding - More Exercise/CelsiusТoFahrenheit.js
@@ -0,0 +1,8 @@
+function CelsiusToFahrenheit(input) {
+    let celsius = Number(input[0]);
+    let fahrenheit = celsius * 1.8 + 32;
+    console.log(fahrenheit.toFixed(2));
+
+}
+
+CelsiusToFahrenheit([25])
---
+++ First Ster in Coding - More Exercise/WeatherForecast.js
@@ -0,0 +1,10 @@
+function WeatherForecast(input) {
+
+    if (input[0] == "sunny") {
+        console.log("It's warm outside!")
+    } else {
+        console.log("It's cold outside!")
+    }
+}
+
+WeatherForecast(["sunny"])
