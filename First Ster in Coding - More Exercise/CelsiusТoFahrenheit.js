935d14ec12f197f7c2f323cc716e12be2d339809
ndimitrova <nninata@abv.bg>
2023-01-30 21:43:36 +0200

    First Step in Coding - More Exercise

--- First Ster in Coding - More Exercise/CelsiusТoFahrenheit.js
+++
@@ -1,8 +0,0 @@
-function CelsiusToFahrenheit(input) {
-    let celsius = Number(input[0]);
-    let fahrenheit = celsius * 1.8 + 32;
-    console.log(fahrenheit.toFixed(2));
-
-}
-
-CelsiusToFahrenheit([25])
